e156c454cc93c2b9faf48b220e1f3ebdf12a4692
AnilKumarBandi27 <40137206+AnilKumarBandi27@users.noreply.github.com>
2020-01-24 13:12:23 +0530

    Create conn.py

---
+++ conn.py
@@ -0,0 +1,72 @@
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+
+# Constants for IBM COS values
+COS_ENDPOINT = "https://s3.us.cloud-object-storage.appdomain.cloud"
+COS_API_KEY_ID = "uClgWYDiEXBcSAWRSdF29Sq1wr11aCmQy5oEOrEv5VbF"
+COS_AUTH_ENDPOINT = "https://iam.cloud.ibm.com/identity/token"
+COS_RESOURCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/b6c597e9c7804d2b9ab48afbe94726ab:1b09bb96-192c-4d07-b041-b54212b0cc6e::"
+# Create resource
+cos = ibm_boto3.resource("s3",
+    ibm_api_key_id=COS_API_KEY_ID,
+    ibm_service_instance_id=COS_RESOURCE_CRN,
+    ibm_auth_endpoint=COS_AUTH_ENDPOINT,
+    config=Config(signature_version="oauth"),
+    endpoint_url=COS_ENDPOINT
+)
+print ("Connection Established")
+
+def get_buckets():
+    print("Retrieving list of buckets")
+    try:
+        buckets = cos.buckets.all()
+        for bucket in buckets:
+            print("Bucket Name: {0}".format(bucket.name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve list buckets: {0}".format(e))
+
+get_buckets();
+
+def upload_large_file(bucket_name, item_name, file_path):
+    print("Starting large file upload for {0} to bucket: {1}".format(item_name, bucket_name))
+
+    # set the chunk size to 5 MB
+    part_size = 1024 * 1024 * 5
+
+    # set threadhold to 5 MB
+    file_threshold = 1024 * 1024 * 5
+
+    # Create client connection
+    cos_cli = ibm_boto3.client("s3",
+        ibm_api_key_id=COS_API_KEY_ID,
+        ibm_service_instance_id=COS_RESOURCE_CRN,
+        ibm_auth_endpoint=COS_AUTH_ENDPOINT,
+        config=Config(signature_version="oauth"),
+        endpoint_url=COS_ENDPOINT
+    )
+
+    # set the transfer threshold and chunk size in config settings
+    transfer_config = ibm_boto3.s3.transfer.TransferConfig(
+        multipart_threshold=file_threshold,
+        multipart_chunksize=part_size
+    )
+
+    # create transfer manager
+    transfer_mgr = ibm_boto3.s3.transfer.TransferManager(cos_cli, config=transfer_config)
+
+    try:
+        # initiate file upload
+        future = transfer_mgr.upload(file_path, bucket_name, item_name)
+
+        # wait for upload to complete
+        future.result()
+
+        print ("Large file upload complete!")
+    except Exception as e:
+        print("Unable to complete large file upload: {0}".format(e))
+    finally:
+        transfer_mgr.shutdown()
+
+upload_large_file("anil123anil123","backup.yaml","/root/backup.yaml");
