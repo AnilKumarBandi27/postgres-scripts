ff42c0f97d70491ba5d1c236db5e2e79b7a50fac
AnilKumarBandi27 <40137206+AnilKumarBandi27@users.noreply.github.com>
2020-01-27 18:32:05 +0530

    Update con.py

--- con.py
+++ con.py
@@ -14,15 +14,14 @@ cos = ibm_boto3.resource("s3",
     config=Config(signature_version="oauth"),
     endpoint_url=COS_ENDPOINT
 )
-
-
 def get_item(bucket_name, item_name):
+    print("Retrieving item from bucket: {0}, key: {1}".format(bucket_name, item_name))
     try:
         file = cos.Object(bucket_name, item_name).get()
-        print("{0}".format(file["Body"].read()))
+        print("File Contents: {0}".format(file["Body"].read()))
     except ClientError as be:
         print("CLIENT ERROR: {0}\n".format(be))
     except Exception as e:
         print("Unable to retrieve file contents: {0}".format(e))
-
-get_item(os.environ['BUCKET_NAME'],"sample.sql");
+        
+get_item("anil123anil123","sample.sql");
