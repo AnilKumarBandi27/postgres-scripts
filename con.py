1e9f76b5e0b2929508bd0d73ae9a6bd5c45c66a6
AnilKumarBandi27 <40137206+AnilKumarBandi27@users.noreply.github.com>
2020-01-27 14:10:43 +0530

    Update con.py

--- con.py
+++ con.py
@@ -5,8 +5,8 @@ from ibm_botocore.client import Config, ClientError
 # Constants for IBM COS values
 COS_ENDPOINT = os.environ['ENDPOINT' ]
 COS_API_KEY_ID = os.environ['API_KEY']
-COS_AUTH_ENDPOINT = os.environ['AUTH_ENDPOINT'] 
-COS_RESOURCE_CRN = os.environ['RESOURCE_CRN'] 
+COS_AUTH_ENDPOINT = os.environ['AUTH_ENDPOINT']
+COS_RESOURCE_CRN = os.environ['RESOURCE_CRN']
 cos = ibm_boto3.resource("s3",
     ibm_api_key_id=COS_API_KEY_ID,
     ibm_service_instance_id=COS_RESOURCE_CRN,
@@ -14,26 +14,12 @@ cos = ibm_boto3.resource("s3",
     config=Config(signature_version="oauth"),
     endpoint_url=COS_ENDPOINT
 )
-print ("Connection Established")
 
-def get_buckets():
-    print("Retrieving list of buckets")
-    try:
-        buckets = cos.buckets.all()
-        for bucket in buckets:
-            print("Bucket Name: {0}".format(bucket.name))
-    except ClientError as be:
-        print("CLIENT ERROR: {0}\n".format(be))
-    except Exception as e:
-        print("Unable to retrieve list buckets: {0}".format(e))
-
-get_buckets();
 
 def get_item(bucket_name, item_name):
-    print("Retrieving item from bucket: {0}, key: {1}".format(bucket_name, item_name))
     try:
         file = cos.Object(bucket_name, item_name).get()
-        print("File Contents: {0}".format(file["Body"].read()))
+        print("{0}".format(file["Body"].read()))
     except ClientError as be:
         print("CLIENT ERROR: {0}\n".format(be))
     except Exception as e:
