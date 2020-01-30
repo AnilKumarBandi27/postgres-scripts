0cf2208ad0d6711497fee76204acbc6732c675e2
AnilKumarBandi27 <40137206+AnilKumarBandi27@users.noreply.github.com>
2020-01-30 23:03:52 +0530

    Update getNames.sh

--- getNames.sh
+++ getNames.sh
@@ -2,7 +2,7 @@
 
 IFS=';' read -ra NAMES <<< "$DB_NAMES"
 
-wget https://github.com/AnilKumarBandi27/postgres-scripts/blob/master/backup-script.py
+wget https://raw.githubusercontent.com/AnilKumarBandi27/postgres-scripts/master/backup-script.py
 
 for i in "${NAMES[@]}"; do
    PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $SERVICE_IP   -U $POSTGRES_USER   -d  $i > "$i-`date +"%d-%m-%Y"`.sql"
