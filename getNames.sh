440f6c4dfc8f4f02ea254133646ae39f675583a4
AnilKumarBandi27 <40137206+AnilKumarBandi27@users.noreply.github.com>
2020-01-30 22:07:52 +0530

    Update getNames.sh

--- getNames.sh
+++ getNames.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-IFS=';' read -ra NAMES <<< "$STR"    #Convert string to array
+IFS=';' read -ra NAMES <<< "$DB_NAMES"
 
-#Print all names from array
+wget https://raw.githubusercontent.com/deekshahegde86/postgresql-operator-phases-1-2-3/master/scripts/backup/backup-script.py
+
 for i in "${NAMES[@]}"; do
-    echo $i
+   PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $SERVICE_IP   -U $POSTGRES_USER   -d  $i > "$i-`date +"%d-%m-%Y"`.sql"
+   export DB_NAME="$i-`date +"%d-%m-%Y"`.sql"
+   python backup-script.py
+   rm -rf DB_NAME
 done
